aa4fa1fab5bf38dc8f0d178f5b51a8f5dc22531d
TheMindfulMoron <ivanthetruespacebiker@gmail.com>
2022-01-29 16:55:01 -0800

    Initial commit

---
+++ src/main/java/net/themindfulmoron/test/TalonModTest.java
@@ -0,0 +1,17 @@
+package net.themindfulmoron.test;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TalonModTest implements ModInitializer {
+
+	public static final String MOD_ID = "testmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
